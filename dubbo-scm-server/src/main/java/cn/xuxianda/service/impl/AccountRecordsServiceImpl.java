f757beab10b674a8a2d92ac65f4b787aecf63cce
Xianda Xu <Xianda Xu@DESKTOP-A98DQ99>
2017-02-21 22:43:19 +0800

    asdf

---
+++ dubbo-scm-server/src/main/java/cn/xuxianda/service/impl/AccountRecordsServiceImpl.java
@@ -0,0 +1,11 @@
+package cn.xuxianda.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import cn.xuxianda.entity.AccountRecords;
+import cn.xuxianda.service.AccountRecordsService;
+
+@Service("accountRecordsService")
+public class AccountRecordsServiceImpl extends BaseServiceImpl<AccountRecords> implements AccountRecordsService {
+
+}
